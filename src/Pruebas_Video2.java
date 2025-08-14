d42d558e380b969efbfaf1ab0e653a59004fccff
John Acosta <johnalejandroacostaespejo@gmail.com>
2025-08-13 19:53:57 -0500

    Agregamos el ejerciocio a resolver

--- src/Pruebas_Video2.java
+++ src/Pruebas_Video2.java
@@ -1,7 +1,25 @@
-public class Pruebas_Video2 {
-        public static void main(String[] args) {
+public class OperatorsExercises {
 
-            
+    public static void main(String[] args) {
 
-        }
+        // 1. Crea una variable con el resultado de cada operación aritmética.
+
+        // 2. Crea una variable para cada tipo de operación de asignación.
+
+        // 3. Imprime 3 comparaciones verdaderas con diferentes operadores de comparación.
+
+        // 4. Imprime 3 comparaciones falsas con diferentes operadores de comparación.
+
+        // 5. Utiliza el operador lógico and.
+
+        // 6. Utiliza el operador lógico or.
+
+        // 7. Combina ambos operadores lógicos.
+
+        // 8. Añade alguna negación.
+
+        // 9. Imprime 3 ejemplos de uso de operadores unarios.
+
+        // 10. Combina operadores aritméticos, de comparación y lógicos.
+    }
 }
